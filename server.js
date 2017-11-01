a4a4c2895d2fbad01547edc16898daf6f5b1ec2f
James <captainjimscharters@gmail.com>
2017-11-01 22:53:32 +0000

    thinks

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Dependencies
- var express = require("express");
- var exphbs = require("express-handlebars");
+var express = require("express");
+var exphbs = require("express-handlebars");
 var path = require('path');
 var bodyParser = require("body-parser");
 var favicon = require('serve-favicon');
@@ -18,7 +18,7 @@ var connection = mysql.createConnection({
  var app = express();
 //
 // // Specify the port.
- var port = 9000;
+var port= normalizePort(process.env.PORT || '9000');
 //
 // // Set Handlebars as the default templating engine.
  app.engine("handlebars", exphbs({ 
@@ -34,13 +34,11 @@ app.use(bodyParser.urlencoded({
 //
 //
 //             // Routes
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
 			connection.query("SELECT * FROM ready WHERE ?",{is_eaten: 0},(error,notEaten) => {
 			if (error) throw error;
 			connection.query("SELECT * FROM ready WHERE ?",{is_eaten: 1},(error,isEaten) => {
 			if (error) throw error;
-			console.log(JSON.stringify(notEaten));
-			 console.log(JSON.stringify(isEaten));
 			return res.render("burger",{
 			title: "Boogers, Not Burgers",	
 			notEaten: notEaten,
@@ -52,36 +50,23 @@ app.get("/", function(req, res) {
     });
 
 
-	app.post("/", (req,res,error) => {
+	app.post("/", (req,res) => {
 
-	//	if(error) throw error;
 		var newBurger=req.body;
-		console.log(newBurger)
-		connection.query("INSERT INTO ready (burger_name) VALUES (?)",[newBurger], function(err, result) {	
-//	connection.query("INSERT INTO ready (burger_name,user_created,date_created) VALUES (?)",{burger_name: newBurger.burger_name, user_created: newBurger.user_created,date_created: newBurger.date_created}, function(err, result) {
-  		  if (err) {throw err
-   		   //return res.status(500).end();
-    			}
-//	res.json(result);
-	console.log(result);
-    // Send back the ID of the new todo
-   // res.json({ id: result.insertId });
-    //console.log({ id: result.insertId });
+	connection.query("INSERT INTO ready SET ?",{burger_name: newBurger.burger_name, user_created: newBurger.user_created,is_eaten: 0 }, (err, result) => {
+  		  if (err) throw err
+	res.redirect("/");
   });
 		
 
 });
-        app.post("/eatburger/:id", (req,res,error) => {
-
-                if(error) throw error;
+        app.post("/eatburger/:id", (req,res) => {
+		console.log(req.params.id)
                 var eatBurger=req.params.id;
-		var timeStamp=moment().format();
-		connection.query("UPDATE ready SET ? WHERE ?",{is_eaten:1,date_eaten: timeStamp},{is_eaten:0,id: eatBurger},(error,results)=> {
+		connection.query("UPDATE ready SET is_eaten = ? WHERE id ="+eatBurger,{is_eaten:1},(error,results)=> {
+			console.log(results);
 			if (error) throw error;
-			else {
 			return res.redirect("/");
-}			
-	//put sql here DUH
 
 });
 });
